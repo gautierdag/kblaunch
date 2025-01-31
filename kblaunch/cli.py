f7301030a8c8adec1c697281687760eb98fbf313
Gautier Dagan <s2234411@ed.ac.uk>
2025-01-31 10:39:51 +0000

    tunnel setup script

--- kblaunch/cli.py
+++ kblaunch/cli.py
@@ -12,6 +12,13 @@ from kubernetes import client, config
 from kubernetes.client.rest import ApiException
 from loguru import logger
 
+
+from kblaunch.bash_utils import (
+    install_vscode_command,
+    send_message_command,
+    setup_git_command,
+)
+
 MAX_CPU = 192
 MAX_RAM = 890
 MAX_GPU = 8
@@ -185,20 +192,6 @@ def create_git_secret(
     except Exception as e:
         logger.error(f"Error creating Git secret: {e}")
         return False
-
-
-def setup_git_command() -> str:
-    """Generate command to setup Git with SSH key."""
-    return (
-        """apt-get update && apt-get install -y git openssh-client && """
-        """mkdir -p ~/.ssh && """
-        """cp /etc/ssh-key/ssh-privatekey ~/.ssh/id_rsa && """
-        """chmod 600 ~/.ssh/id_rsa && """
-        """ssh-keyscan github.com >> ~/.ssh/known_hosts && """
-        """git config --global core.sshCommand 'ssh -i ~/.ssh/id_rsa' && """
-        """git config --global user.name "${USER}" && """
-        """git config --global user.email "${GIT_EMAIL}" && """
-    )
 
 
 class KubernetesJob:
@@ -529,38 +522,6 @@ def check_if_completed(job_name: str, namespace: str = "informatics") -> bool:
         if is_completed:
             delete_namespaced_job_safely(job_name, namespace)
     return is_completed
-
-
-def send_message_command(env_vars: set) -> str:
-    """
-    Send a message to Slack when the job starts if the SLACK_WEBHOOK environment variable is set.
-    """
-    if "SLACK_WEBHOOK" not in env_vars:
-        logger.debug("SLACK_WEBHOOK not found in env_vars.")
-        return ""
-
-    message_json = json.dumps(
-        {
-            "text": "Job started in ${POD_NAME}. To connect to the pod, run ```kubectl exec -it ${POD_NAME} -- /bin/bash```"
-        }
-    )
-    # escape double quotes, backticks, and newlines
-    message_json = (
-        message_json.replace('"', '\\"').replace("`", "\\`").replace("\n", "\\n")
-    )
-    return (
-        """apt-get update && apt-get install -y curl;"""  # Install the curl command
-        + f"""curl -X POST -H 'Content-type: application/json' --data "{message_json}" $SLACK_WEBHOOK ;"""
-    )
-
-
-def install_vscode_command() -> str:
-    """Generate command to install VS Code CLI."""
-    return (
-        """curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64' --output vscode_cli.tar.gz && """
-        """tar -xf vscode_cli.tar.gz && """
-        """rm vscode_cli.tar.gz && """
-    )
 
 
 def get_env_vars(
